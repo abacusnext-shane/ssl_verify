28ad0ab3dc76082d6943744f44bb56fe73de944e
abacusnext-shane <69823206+abacusnext-shane@users.noreply.github.com>
2020-12-03 13:35:10 -0800

    Update testssl.js

--- testssl.js
+++ testssl.js
@@ -2,11 +2,11 @@ let ssllabs = require('node-ssllabs');
 let dnstoscan = process.env.DNSTOSCAN; // add your variable i.e. google.com
 
 ssllabs.scan(dnstoscan, function (err, host) {
+	console.dir(`full results: ${host}`);
 	let res = host.endpoints[0].details.certChains[0].issues;
 	let grade = host.endpoints[0].grade;
 	console.dir(`Certificate Chain issues for ${dnstoscan} = ${res}`);
 	console.dir(`grade = ${grade}`);
-	console.dir(`full results: ${host}`);
 	return process.exit(res + (grade === 'A') ? 0 : 1);
 });
 // expected results = 0
